6caacc7bba4404d64d5ca085595bab77215d4cc5
leo <leolefebvre.contact@gmail.com>
2016-01-30 14:56:08 +0100

    WIP : work on combat

--- Assets/Scripts/Characters/Minions.cs
+++ Assets/Scripts/Characters/Minions.cs
@@ -2,19 +2,30 @@
 using System.Collections;
 using System;
 
+public enum MinionState
+{
+    stop,
+    moving,
+    fighting
+}
+
 public class Minions : MonoBehaviour
 {
     public MinionsInformations minionsInformations;
-    public int ownerNumber { get; private set; }
     public int playerNumber;
+    public bool DEBUGFightInitator = false;
+
+    public MinionState state { get; protected set; }
+    public int ownerNumber { get; private set; }
+    
+    public Minions opponent { get; private set; }
 
     private NavMeshAgent navAgent;
     private Transform goal;
-
     private bool isInit = false;
-
-    private GameObject healthBar;
-
+    private int currentLife;
+    private GameObject lifeBar;
+    
     // Use this for initialization
     protected void Start () {
         initalize();
@@ -28,16 +39,28 @@ public class Minions : MonoBehaviour
         }
 
         navAgent = GetComponent<NavMeshAgent>();
-
         playerNumber = GameObject.Find("GameSharedData").GetComponent<GameSharedData>().PlayerNumber;
+        lifeBar = transform.FindChild("LifeBar").gameObject;
+        currentLife = minionsInformations.baseLifePoints;
 
-        healthBar = transform.FindChild("HealthBar").gameObject;
+        state = MinionState.stop;
     }
 
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Minion")
+        {
+            if (other.GetComponent<Minions>().opponent != this)
+            {
+                LaunchFight(other.GetComponent<Minions>());
+            }
+        }
+    }
 
     public void SetOwnerNumber(int owner)
     {
@@ -69,5 +92,54 @@ public class Minions : MonoBehaviour
         }
     }
 
+    public void LaunchFight(Minions otherFighter)
+    {
+        if(state == MinionState.fighting)
+        {
+            return;
+        }
 
+        Debug.Log("LaunchFight");
+
+        DEBUGFightInitator = true;
+
+        opponent = otherFighter;
+        opponent.opponent = this;
+
+        setupFight();
+        opponent.setupFight();
+        
+        Invoke("Attack", minionsInformations.firstAttackSpeed);
+    }
+
+    public void setupFight()
+    {
+        state = MinionState.fighting;
+        navAgent.Stop();
+    }
+
+    public void Attack()
+    {
+        
+    }
+
+    public int computeDamages()
+    {
+        return minionsInformations.damages;
+    }
+
+    public void TakeDamage(int damages)
+    {
+        currentLife -= damages;
+
+        lifeBar.transform.localScale = new Vector3(lifeBar.transform.localScale.x,
+                                                   lifeBar.transform.localScale.y,
+                                                   (minionsInformations.baseLifePoints - currentLife) / minionsInformations.baseLifePoints);
+    }
+
+
+    public void Die()
+    {
+
+    }
 }
--- Assets/Scripts/ScriptablesObjects/MinionsInformations.cs
+++ Assets/Scripts/ScriptablesObjects/MinionsInformations.cs
@@ -4,11 +4,14 @@ using System.Collections;
 [System.Serializable]
 public class MinionsInformations : ScriptableObject
 {
-    public int baseLifePoint;
+    public int baseLifePoints;
 
     [Tooltip("In seconds")]
     public float attackSpeed;
-    
+
+    [Tooltip("first attack in each fight, in seconds")]
+    public float firstAttackSpeed;
+
     public int damages;
 
     [Tooltip("Damage dealt to units weak againts this unit")]
