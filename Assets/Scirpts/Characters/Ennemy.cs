4d5d824ad833da24952b675fd023a6735a7349ff
leo <leolefebvre.contact@gmail.com>
2016-01-30 10:21:36 +0100

    Add base ennemy behavior : go to a goal

---
+++ Assets/Scirpts/Characters/Ennemy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ennemy : MonoBehaviour
+{
+    public Transform goal;
+
+    private NavMeshAgent navAgent;
+
+	// Use this for initialization
+	protected void Start () {
+        navAgent = GetComponent<NavMeshAgent>();
+        navAgent.destination = goal.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
