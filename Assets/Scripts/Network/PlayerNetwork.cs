50d9ab2f7182ea1d3b1321bac5a77c39fd45d322
tentacode <contact@gabrielpillet.com>
2016-01-30 14:37:22 +0100

    Player camera

--- Assets/Scripts/Network/PlayerNetwork.cs
+++ Assets/Scripts/Network/PlayerNetwork.cs
@@ -1,23 +1,39 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class PlayerNetwork : MonoBehaviour
+public class PlayerNetwork : NetworkBehaviour
 {
-    private int playerIndex;
+    [SyncVar]
+    public int playerIndex;
     
 	void Awake () 
     {
 	   DontDestroyOnLoad(gameObject);
 	}
     
-    public void SetPlayerIndex(int idx)
+    public void InitGame()
     {
-        playerIndex = idx;
+        InitPosition();
+        InitCamera();
     }
     
-    public void InitPosition()
+    void InitPosition()
     {
         var ground = GameObject.Find("Ground" + playerIndex);
         transform.position = ground.transform.position;
+    }
+    
+    void InitCamera()
+    {
+        Debug.Log("Init camera");
+        GameObject cameraTarget = GameObject.Find("CameraTarget");
+        cameraTarget.transform.position = transform.position;
+        cameraTarget.transform.rotation = transform.rotation;
+    }
+    
+    void Update()
+    {
+        gameObject.name = "Player"+playerIndex;
     }
 }
