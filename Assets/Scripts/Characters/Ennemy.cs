d228d0eed238911b4eb5086db7dd87f6c32516ba
leo <leolefebvre.contact@gmail.com>
2016-01-30 11:59:31 +0100

    Add base enemy behavior, refacto Prefab/Character and Scripts folders

---
+++ Assets/Scripts/Characters/Ennemy.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Ennemy : MonoBehaviour
+{
+    public int ownerNumber { get; private set; }
+    public int playerNumber;
+
+    private NavMeshAgent navAgent;
+    private Transform goal;
+
+    private bool isInit = false;
+
+    // Use this for initialization
+    protected void Start () {
+        initalize();
+    }
+	
+    protected void initalize()
+    {
+        if(isInit)
+        {
+            return;
+        }
+
+        navAgent = GetComponent<NavMeshAgent>();
+
+        if (playerNumber <= 0)
+        {
+            playerNumber = GameObject.Find("GameSharedData").GetComponent<GameSharedData>().PlayerNumber;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetOwnerNumber(int owner)
+    {
+        if(!isInit)
+        {
+            initalize();
+        }
+
+        ownerNumber = owner;
+        
+        SetGoal(GameObject.Find("Player" + ((ownerNumber%playerNumber) + 1)).transform);
+    }
+
+    public void SetGoal(Transform goalTransform)
+    {
+        if (!isInit)
+        {
+            initalize();
+        }
+
+        goal = goalTransform;
+        if (goal != null)
+        {
+            navAgent.destination = goal.position;
+        }
+        else
+        {
+            navAgent.Stop();
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/SpawnerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerController : MonoBehaviour
+{
+    #region parameters
+    public int ownerNumber;
+    public Ennemy spawnedCharacter;
+    public AnimationCurve spawnRateCurve;
+    public float TimeBeforeFirstLaunch;
+    public float maxTime;
+
+    private float beginTime;
+
+    #endregion
+    
+    // Use this for initialization
+    void Start () {
+        Invoke("FirstSpawn", TimeBeforeFirstLaunch);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float x = spawnRateCurve.Evaluate(Time.time - beginTime);
+	}
+
+    private void FirstSpawn()
+    {
+        beginTime = Time.time;
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        Ennemy ennemy = Instantiate(spawnedCharacter, transform.position, transform.rotation) as Ennemy;
+        
+        ennemy.SetOwnerNumber(ownerNumber);
+
+
+        Invoke("Spawn", spawnRateCurve.Evaluate((Time.time - beginTime) / maxTime));
+    }
+}
