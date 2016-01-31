ff4c3b898a010cac0c4e993200b2d42a562ec82f
leo <leolefebvre.contact@gmail.com>
2016-01-31 10:39:13 +0100

    HIGH SCHOOL MUSIC HALL BITECHES !!!

---
+++ Assets/Scripts/Sound/Music/MusicalElement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class MusicalElement : MonoBehaviour
+{
+    protected MusicManager musicManager;
+
+    [SerializeField]
+    protected bool loop;
+
+    protected virtual void Start()
+    {
+        setupMusicManager();
+    }
+
+    protected void setupMusicManager()
+    {
+        musicManager = GameObject.Find("MusicManager").GetComponent<MusicManager>();
+    }
+
+    public virtual void LaunchMusic(AudioClip Music, AudioMixerGroup mixerGroup)
+    {
+        if (musicManager == null)
+        {
+            setupMusicManager();
+        }
+
+        musicManager.PlayMusic(Music, mixerGroup, loop);
+    }
+}
---
+++ Assets/Scripts/Sound/Music/MusicalOnStart.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicalOnStart : MusicalMono
+{
+    override protected void Start()
+    {
+        base.Start();
+        playSound();
+    }
+}
