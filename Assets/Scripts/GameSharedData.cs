cda013055009415dd9d0681ed910621f6ce42537
Maelyss Farges <farges.maelyss@gmail.com>
2016-01-31 06:23:06 +0100

    Movement Order + Multiplayer

--- Assets/Scripts/GameSharedData.cs
+++ Assets/Scripts/GameSharedData.cs
@@ -3,12 +3,46 @@ using System.Collections;
 
 public class GameSharedData : MonoBehaviour
 {
-    public int NumberOfPlayer
+    public static GameObject[] GetAllPlayers
     {
         get
         {
-            return GameObject.FindGameObjectsWithTag("Player").Length;
+            return GameObject.FindGameObjectsWithTag("Player");
         }
+    }
+
+    public static int NumberOfPlayer
+    {
+        get
+        {
+            return GetAllPlayers.Length;
+        }
+    }
+
+    public static GameObject GetPlayerNumberNext(Unit_ID playerInit, int numberToIncrement)
+    {
+        // Avoid overlaping yourself
+        numberToIncrement = Mathf.Clamp(numberToIncrement, 0, GameSharedData.NumberOfPlayer - 1);
+
+        Debug.Log(numberToIncrement);
+        int pos = 0;
+        // Find your position in the table
+        foreach (var v in GetAllPlayers)
+        {
+            if (v.GetComponent<Unit_ID>().GetPlayerIndex() == playerInit.GetPlayerIndex())
+            {
+                break;
+            }
+            pos++;
+        }
+
+        Debug.Log(pos);
+
+        int playerId = (pos + numberToIncrement) % (GameSharedData.NumberOfPlayer);
+
+        Debug.Log(playerId);
+
+        return GetAllPlayers[playerId];
     }
     /*
     public bool autoCountPlayers;
