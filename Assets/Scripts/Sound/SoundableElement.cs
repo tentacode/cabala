ea04b99c9078c210d597687bb96bd2283f957f77
leo <leolefebvre.contact@gmail.com>
2016-01-31 08:19:49 +0100

    minions have sound, update lobby with sound manager fuck yeah nigga toi même tu sais BUILD UN INFO NIQUE TOI

--- Assets/Scripts/Sound/SoundableElement.cs
+++ Assets/Scripts/Sound/SoundableElement.cs
@@ -4,14 +4,26 @@ using UnityEngine.Audio;
 
 public class SoundableElement : MonoBehaviour
 {
+    [SerializeField]
     protected SoundManager soundManager;
 	
 	protected virtual void Start () {
-        soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+        setupSoundManager();
 	}
+
+    protected void setupSoundManager()
+    {
+        soundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
+    }
 
     public virtual void LaunchSound(AudioClip sound, AudioMixerGroup mixerGroup)
     {
+        if(soundManager == null)
+        {
+            setupSoundManager();
+        }
+
+        Debug.Log("sound is " + (sound == null) + " || mixer is  " + (mixerGroup == null) + " || soundManager is " + (soundManager == null));
         soundManager.PlaySound(sound, mixerGroup);
     }
 }
