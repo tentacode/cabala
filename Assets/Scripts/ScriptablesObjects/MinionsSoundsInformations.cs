4f2caac866204a445e398dbbf0a430ec28c48222
leo <leolefebvre.contact@gmail.com>
2016-01-31 06:46:24 +0100

    Update red ground texture and set sound classes

--- Assets/Scripts/ScriptablesObjects/MinionsSoundsInformations.cs
+++ Assets/Scripts/ScriptablesObjects/MinionsSoundsInformations.cs
@@ -5,4 +5,16 @@ using System.Collections.Generic;
 public class MinionsSoundsInformations : ScriptableObject
 {
     public MinionSounds[] minionSounds;
+
+    public MinionSounds getAccordingMinionSounds(MinionType minionType)
+    {
+        for(int i = 0; i < minionSounds.Length; i++)
+        {
+            if(minionSounds[i].minionType == minionType)
+            {
+                return minionSounds[i];
+            }
+        }
+        return null;
+    }
 }
